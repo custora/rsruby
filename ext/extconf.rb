d0d95dcd8203b09991cbfc7bf780eb9fc73c1aaa
Andrew Lim <andy@custora.com>
2017-10-25 12:04:23 -0400

    Rely on `R RHOME` and `R CMD` for Makefile settings
    If we don't override with flags like --with-R-lib:
    
    - Use `R RHOME` to set RSRUBY_R_HOME
    - Use `R CMD` to set library/include flags, using append_cppflags and
      append_ldflags
    
    Also rename config.h to r_config.h, to make its filename consistent with
    the R_CONFIG_H definition.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,60 +1,34 @@
 require 'mkmf'
 
-# Searched after ENV['PATH']
-DEFAULT_R_PATHS = %w(
-  /usr/local/lib64/R
-  /usr/local/lib/R
-  /usr/lib64/R
-  /usr/lib/R
-  /Library/Frameworks/R.framework/Resources
-  /usr/local/Frameworks/R.framework/Resources
-)
-
-# Searched after DEFAULT_R_PATHS/include
-DEFAULT_R_INCLUDE_PATHS = %w(
-  /usr/include/R
-  /usr/share/R/include
-)
-
 dir_config('R')
 
-r_paths = if $configure_args.has_key?('--with-R-dir')
-  [$configure_args['--with-R-dir']]
+if $configure_args.has_key?('--with-R-lib')
+  find_library('R', nil, $configure_args['--with-R-lib'])
+  unless have_library('R')
+    $stderr.puts "\nERROR: Cannot find the R library, aborting."
+    exit 1
+  end
 else
-  r_paths = ENV['PATH'].split(File::PATH_SEPARATOR) + DEFAULT_R_PATHS
+  append_ldflags(`R CMD config --ldflags`.shellsplit)
 end
 
-r_lib_paths = if $configure_args.has_key?('--with-R-lib')
-  [$configure_args['--with-R-lib']]
+if $configure_args.has_key?('--with-R-include')
+  find_header('R.h', nil, $configure_args['--with-R-include'])
+  unless have_header('R.h')
+    $stderr.puts "\nERROR: Cannot find the R header, aborting."
+    exit 1
+  end
 else
-  r_paths.map { |dir| File.join(dir, 'lib') }
+  append_cppflags(`R CMD config --cppflags`.shellsplit)
 end
 
-find_library('R', nil, *r_lib_paths)
-unless have_library('R')
-  $stderr.puts "\nERROR: Cannot find the R library, aborting."
-  exit 1
-end
-
-r_include_paths = if $configure_args.has_key?('--with-R-include')
-  [$configure_args['--with-R-include']]
-else
-  r_paths.map { |dir| File.join(dir, 'include') } + DEFAULT_R_INCLUDE_PATHS
-end
-
-find_header('R.h', nil, *r_include_paths)
-unless have_header('R.h')
-  $stderr.puts "\nERROR: Cannot find the R header, aborting."
-  exit 1
-end
-
-File.open("config.h", "w") do |f|
+File.open("r_config.h", "w") do |f|
   f.puts("#ifndef R_CONFIG_H")
   f.puts("#define R_CONFIG_H")
-  r_home = $configure_args.has_key?('--with-R-dir') ? $configure_args['--with-R-dir'].inspect : 'NULL'
-  f.puts("#define RSRUBY_R_HOME #{r_home}")
+  r_home = $configure_args.has_key?('--with-R-dir') || `R RHOME`.chomp
+  f.puts("#define RSRUBY_R_HOME \"#{r_home}\"")
   f.puts("#endif")
 end
-$extconf_h = 'config.h'
+$extconf_h = 'r_config.h'
 
 create_makefile("rsruby_c")
